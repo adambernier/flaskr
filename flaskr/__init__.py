9ad490a6113dd83c4faef7467aa2b56666c71e89
Adam C Bernier <adam.c.bernier@gmail.com>
2019-12-16 14:17:44 -0800

    cache issue

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -75,6 +75,7 @@ def create_app(test_config=None):
     @app.after_request
     def add_header(response):
         response.cache_control.max_age = 0
+        response.cache_control.public = True
         return response
 
 app = create_app()
