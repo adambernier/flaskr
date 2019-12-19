851f4615e0ecd1faf1b829a36c4605a8bf85a898
Adam C Bernier <adam.c.bernier@gmail.com>
2019-12-19 13:42:11 -0800

    trying something else

--- bin/pg_backup_to_s3_v4.sh
+++ bin/pg_backup_to_s3_v4.sh
@@ -5,51 +5,25 @@ set -o pipefail
 
 # Download the latest backup from
 # Heroku and gzip it
-
 heroku pg:backups:download --output=/tmp/pg_backup.dump --app $APP_NAME
 gzip /tmp/pg_backup.dump
 
 # Encrypt the gzipped backup file
 # using GPG passphrase
-
 gpg --yes --batch --passphrase=$PG_BACKUP_PASSWORD -c /tmp/pg_backup.dump.gz
 
 # Remove the plaintext backup file
-
 rm /tmp/pg_backup.dump.gz
 
 # Generate backup filename based
 # on the current date
-
 BACKUP_FILE_NAME="mechanical-meat-database-backup-$(date '+%Y-%m-%d_%H.%M').gpg"
+#BACKUP_FILE_NAME="pg_backup_to_s3_v4.sh"
 
-# Make sure to use the UTC
-# date for S3 signature!
-
-#DATE=`date -R -u`
-
-#S3_PATH="/${S3_BUCKET}/${BACKUP_FILE_NAME}"
-
-# Generate S3 signature needed
-# to upload file to the bucket
-
-#S3_STRING="PUT\n\napplication/octet-stream\n${DATE}\n${S3_PATH}"
-#S3_SIGNATURE=`echo -en ${S3_STRING} | openssl sha1 -hmac ${S3_SECRET} -binary | base64`
-
-# Upload the file to S3 using
-# the signature auth header
-#curl -X PUT -T "/tmp/pg_backup.dump.gz.gpg" \
-#  -H "Host: ${S3_BUCKET}.s3-us-east-2.amazonaws.com" \
-#  -H "Date: ${DATE}" \
-#  -H "Content-Type: application/octet-stream" \
-#  -H "Authorization: AWS ${S3_KEY}:${S3_SIGNATURE}" \
-#  https://${S3_BUCKET}.s3-us-east-2.amazonaws.com/${BACKUP_FILE_NAME}
-
-# rename file
 mv /tmp/pg_backup.dump.gz.gpg "/tmp/${BACKUP_FILE_NAME}"
 
-#fileLocal="${1:-example-local-file.ext}"
-fileLocal="tmp/${BACKUP_FILE_NAME}"
+fileLocal="${1:-BACKUP_FILE_NAME}"
+#fileLocal="/tmp/${BACKUP_FILE_NAME}"
 bucket="${2:-mechanical-meat-database-backup}"
 region="${3:-}"
 storageClass="${4:-STANDARD}"  # or 'REDUCED_REDUNDANCY'
@@ -73,8 +47,7 @@ m_sed() {
 }
 
 awsStringSign4() {
-  #kSecret="AWS4$1"
-  kSecret="AWS4${fileLocal}"
+  kSecret="AWS4$1"
   kDate=$(printf         '%s' "$2" | m_openssl dgst -sha256 -hex -mac HMAC -macopt "key:${kSecret}"     2>/dev/null | m_sed 's/^.* //')
   kRegion=$(printf       '%s' "$3" | m_openssl dgst -sha256 -hex -mac HMAC -macopt "hexkey:${kDate}"    2>/dev/null | m_sed 's/^.* //')
   kService=$(printf      '%s' "$4" | m_openssl dgst -sha256 -hex -mac HMAC -macopt "hexkey:${kRegion}"  2>/dev/null | m_sed 's/^.* //')
