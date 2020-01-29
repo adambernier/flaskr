d241572c8f73279021e78dda754ec2114fafacf8
Adam C Bernier <adam.c.bernier@gmail.com>
2020-01-29 09:46:35 -0800

    many-to-many primary key

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -42,6 +42,7 @@ CREATE TABLE tag (
 CREATE TABLE post_tag (
   post_id BIGINT NOT NULL,
   tag_id BIGINT NOT NULL,
+  PRIMARY KEY (post_id, tag_id),
   FOREIGN KEY (post_id) REFERENCES post (id),
   FOREIGN KEY (tag_id) REFERENCES tag (id)
 );
